6790dc918e7744e94b56998c1263978aae4dc97a
neimaaman <neimaaman1234@gmail.com>
2024-01-15 23:02:44 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//I will declare two variables
+const numOne = prompt("enter a number");
+const numTwo = 0.6214;
+
+// multiply two variables
+const result = numOne / numTwo;
+const message = "miles to km";
+// I will log it to the console
+console.log(result);
+console.log(message);
+
+const displayMessage = `miles to km : ${result}`;
+console.log(displayMessage);
+
+console.log(typeof result);
+console.log(typeof message);
+
