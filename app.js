5adc5402672b4c6e2ec54eae479d98d04e4d6c33
neimaaman <neimaaman1234@gmail.com>
2024-01-22 11:48:27 -0500

    added how to append a child

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const result = numOne * numTwo;
 console.log(result);
 // console.log(message);
 
-const displayMessage = (`miles to km : ${result}`);
+const displayMessage = `miles to km : ${result}`;
 // console.log(displayMessage);
 
 // console.log(typeof result);
@@ -18,4 +18,20 @@ const displayMessage = (`miles to km : ${result}`);
 const roundNumber = Number (result);
 console.log(result);
 
-alert(`the result is: ${result}`);
+// alert(displayMessage);
+const bodyElement = document.body;
+const newText = document.createElement("div");
+newText.classList.add("h-screen", "flex", "justify-center","items-center");
+    
+
+newText.innerHTML = `
+
+<div class="bg-teal-300 p-8 w-2/3 rounded-lg">
+        <p class="text-gray-600 text-xs uppercase "> miles to km by @neimaaman </p>
+        <p class="text-4xl"> miles to km :<span class="font-bold text-white "> ${result}</span> </p>
+    
+</div>
+`;
+bodyElement.appendChild(newText);
+
+ 
